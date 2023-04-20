81868e11cb431d251857005b87b4e24d8f24c5ad
“lnxtha” <linusxrstha@gmail.com>
2023-04-20 23:09:16 +0000

    Delete Prod files

--- ProjectWeek6/prod/network/conf.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "acs730-week6-sshrestha63"
-    #key    = "01-networking/terraform.tfstate"
-    key    = "prod/01-networking/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- ProjectWeek6/prod/webserver/install_httpd.sh
+++
@@ -1,9 +0,0 @@
-
-              #!/bin/bash
-              echo "Hello Students" > week3acs.txt
-              yum -y update
-              yum -y install httpd
-              echo "<h1>Welcome to ACS730 Week 4!"  >  /var/www/html/index.html
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-           EOF
